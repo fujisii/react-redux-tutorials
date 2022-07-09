90bac5e5e5668582f2065e7f1de6ee01c47339a3
fujisii <51264835+fujisii@users.noreply.github.com>
2022-07-09 20:29:30 +0900

    Provide the Redux Store to React

--- src/index.tsx
+++ src/index.tsx
@@ -2,12 +2,16 @@ import { StrictMode } from "react";
 import * as ReactDOMClient from "react-dom/client";
 
 import App from "./App";
+import store from "./app/store";
+import { Provider } from "react-redux";
 
 const rootElement = document.getElementById("root");
 const root = ReactDOMClient.createRoot(rootElement);
 
 root.render(
   <StrictMode>
-    <App />
+    <Provider store={store}>
+      <App />
+    </Provider>
   </StrictMode>
 );
